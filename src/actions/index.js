2c9db2aa35addf8b4f20f9c43d5f369c6bfd3585
Coenie Beyers <coeniebeyers@gmail.com>
2017-04-04 07:50:25 +0200

    Can now do async request to web3 for fetching an account address from geth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:20010'));
+
 let nextTodoId = 0
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
@@ -14,3 +18,41 @@ export const toggleTodo = (id) => ({
   type: 'TOGGLE_TODO',
   id
 })
+
+let nextAccountId = 0;
+export const REQUEST_NEW_ACCOUNT = 'REQUEST_NEW_ACCOUNT'
+function requestNewAccount(accountName) {
+  return {
+    type: REQUEST_NEW_ACCOUNT
+  }
+}
+
+export const RECEIVE_NEW_ACCOUNT = 'RECEIVE_NEW_ACCOUNT'
+function receiveNewAccount(accountName, accountAddressList) {
+  var accountAddress = 'N/A';
+  if(nextAccountId < accountAddressList.length){
+    accountAddress = accountAddressList[nextAccountId];
+  }
+  console.log('accountName:', accountName);
+  console.log('accountAddress:', accountAddress);
+  return {
+    type: RECEIVE_NEW_ACCOUNT,
+    id: nextAccountId++,
+    accountName,
+    accountAddress
+  }
+}
+
+export function addAccount(accountName){
+  return function(dispatch) {
+
+    dispatch(requestNewAccount(accountName));
+
+    web3.eth.getAccounts(function(err, accountAddressList){
+      if(err){console.log('ERROR:', err);}
+      dispatch(receiveNewAccount(accountName, accountAddressList));
+    })
+  }
+}
+
+

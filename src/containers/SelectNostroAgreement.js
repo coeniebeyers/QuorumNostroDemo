abb85aecce0536087e0bb97cd56b733f748aacb2
Peter Munnings <petermunnings@gmail.com>
2017-04-12 06:47:35 +0200

    Mapped the nostro agreements to a select list - formatting still not working properly

--- src/containers/SelectNostroAgreement.js
+++ src/containers/SelectNostroAgreement.js
@@ -3,11 +3,17 @@ import { updateSelectedNostroAgreement } from '../actions'
 import SelectNostroDropDown from '../components/SelectNostroDropDown'
 
 const formatNostroAgreementListForDropDown = (nostroAgreementList) => {
-	console.log('nostroAgreementList:', nostroAgreementList);
-	//TODO: Need to map the nostro agreements to an array containing value and label
-	return [
-		{value: "1234", label:"JPM USD/ZAR agreement"}
-	]
+  var options=[];
+
+	for(var index=0; index<nostroAgreementList.length; index++){
+		var ag=nostroAgreementList[index];
+		options.push({
+			value: ag.id,
+			label: ag.counterpartiesToCurrency1[0].name + ': ' + ag.currency2 + ' | ' + ag.counterpartiesToCurrency2[0].name + ': ' + ag.currency1
+		});
+	}
+	console.log('options:', options);
+	return options;
 }
 
 const mapStateToProps = (state) => ({

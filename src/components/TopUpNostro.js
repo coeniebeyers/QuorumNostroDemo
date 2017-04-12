352845d7b29d5cf1310e5c54ff5cee7d45084028
Peter Munnings <petermunnings@gmail.com>
2017-04-12 07:14:48 +0200

    Added layout for all the fields

--- src/components/TopUpNostro.js
+++ src/components/TopUpNostro.js
@@ -11,10 +11,30 @@ const TopUpNostro = () => {
 	return (
 		<div>
 			<NavBar />
-			<p>Top up Nostro screen</p> 
-			<div style={fw}>
-				<SelectNostroAgreement />
-			</div>
+			<table>
+				<tbody>
+					<tr>
+						<td style={fw}>Nostro Agreement</td>
+						<td style={fw}><SelectNostroAgreement /></td>
+					</tr>
+					<tr>
+						<td style={fw}>Amount Required (Currency1)</td>
+						<td style={fw}><input /></td>
+					</tr>
+					<tr>
+						<td style={fw}>&nbsp;</td>
+						<td style={fw}><button>Request Rate</button></td>
+					</tr>
+					<tr>
+						<td style={fw}>Amount to pay (Currency2)</td>
+						<td style={fw}>0</td>
+					</tr>
+					<tr>
+						<td style={fw}>&nbsp;</td>
+						<td style={fw}><button>Top up Nostro Account</button></td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	)
 }
